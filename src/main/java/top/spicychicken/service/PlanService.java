4efbd748b0f9d397964859f9bda0616e13b600d5
SpicyChickenFLY <xixiayuchao@gmail.com>
2025-06-25 17:10:38 +0800

    格式化部分文件

--- src/main/java/top/spicychicken/service/PlanService.java
+++ src/main/java/top/spicychicken/service/PlanService.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import top.spicychicken.entity.Plan;
 
-
 public interface PlanService {
     Plan createPlan(Integer taskId, LocalDate planDate);
+
     List<Plan> getPlansByDate(LocalDate date);
+
     List<Plan> getPlansByTask(Integer taskId);
+
     void deletePlan(Integer taskId, LocalDate planDate);
 }
